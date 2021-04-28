c229a77a6a142f92fceb0a84bfac298ef8976c25
yalu-ku <ahroe96@gmail.com>
2021-04-28 23:37:10 +0900

    init

---
+++ SoftwareApp/LoginForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace SoftwareApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 21);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "아이디";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "패스워드";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 27);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "정보추가";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(69, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "정보삭제";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(69, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(9, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = " ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "LoginForm";
+            this.Text = "SWApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SoftwareApp/LoginForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using SoftwareApp.Model;
+
+namespace SoftwareApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tb_userid = textBox1.Text;
+            string tb_userpw = textBox2.Text;
+            int flag = 0;
+
+            Console.WriteLine(tb_userid);
+            Console.WriteLine(tb_userpw);
+            using (var ctx = new ApplicationDataContext())
+            {
+                /*var item = ctx.login.SingleOrDefault(p => p.userid == "id06");
+                item.userpasswd = textBox2.Text;*/
+
+                // 항목의 개수로 로그인 가능 여부 확인
+                var item = ctx.login.Where(a => a.userid == tb_userid && a.userpasswd == tb_userpw);
+                Console.WriteLine("항목의 개수 : " + item.Count());
+                flag = item.Count();
+
+                ctx.SaveChanges();
+            }
+
+            if(flag == 0)
+            {
+                label3.Text = "로그인정보가 일치하지 않습니다.";
+            } 
+            else if (flag == 1)
+            {
+                label3.Text = "로그인 되었습니다.";
+
+                Hide();
+
+                MainForm mf = new MainForm();
+                mf.Show();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e) //정보추가
+        {
+            using (var ctx = new ApplicationDataContext())
+            {
+                var uid = textBox1.Text;
+                var upw = textBox2.Text;
+
+                var item = new tb_logininfo(); //generic type 연동
+                item.userid = uid;
+                ctx.login.Add(항목);
+                ctx.SaveChanges();
+                /*context.Database.Log += (log)->
+                {
+                    Console.WriteLine($"log : {log}");
+                };*/
+                /*Console.WriteLine("데이터베이스 연결");
+                var item = new tb_logininfo();
+                *//*{
+                    userid = "id123",
+                    userpasswd = "pw123"
+                };*/
+                /*context.login.Add(loginInfo);
+                context.SaveChanges();*//*
+                item.userid = "id1234";
+                item.userpasswd = "pw123";
+
+                Console.WriteLine("레코드 정보 생성 완료");
+
+                context.login.Add(item);
+                Console.WriteLine("레코드 추가");
+                context.SaveChanges();
+                Console.WriteLine("커밋!");*/
+
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (var ctx = new ApplicationDataContext())
+            {
+                var item = ctx.login.Where(p => p.dataid >= 1);
+
+                foreach(var i in item)
+                    ctx.login.Remove(i);
+
+                ctx.SaveChanges();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public partial class ApplicationDataContext : DbContext
+        {
+            public ApplicationDataContext() : base("msgDBEntities")
+            {
+            }
+
+            public DbSet<tb_logininfo> login { get; set; }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
